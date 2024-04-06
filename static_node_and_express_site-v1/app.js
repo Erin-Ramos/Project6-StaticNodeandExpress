83a8fa8eb363a12481bca63bad8602a66eb0575a
Erin Ramos <47257106+Erin-Ramos@users.noreply.github.com>
2024-04-05 23:25:39 -0400

    progress made

---
+++ static_node_and_express_site-v1/app.js
@@ -0,0 +1,45 @@
+const express = require('express'); //good
+const bodyParser = require('body-parser'); //good
+const projects = require('./data.json'); 
+
+const app = express(); //good
+
+app.use(bodyParser.urlencoded({extended: false})); //good
+
+
+app.set('view engine', 'pug'); //good 
+app.use('/static', express.static('public')); //good
+
+// set your routes 
+//----------------
+// index to render the home page with locals set to data.projects
+app.get('/', (req, res) => {
+    res.render('index', {projects});
+});
+
+// about to render the about page 
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+// project (5 based on the ID's) to render a page for each project 
+
+app.get('/project/:id', (req, res) => {
+    const project = projects.find( ({id}) => id === +req.params.id);
+    res.render('project', {project})
+});
+
+//-----------------
+// listen on port 3000 
+// log a string to the console stating which port it's listening to 
+app.listen(3000, () => {
+    console.log('This application is running on localhost:3000!')
+}); // good
+
+//-----------------
+// handle errors
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    console.log(res.status(err.status));
+  });  
